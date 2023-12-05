6c13f4e74c959f5002b5fd7a9168a1f5603f9965
JosePaor <paredesortizjosealfredo@gmail.com>
2023-12-05 15:51:02 -0600

    Ext - composition & equilibirum
    Change  the use of composition and equilibrium.

--- flash_1.py
+++ flash_1.py
@@ -1,36 +1,35 @@
 from maingopy import *
-
 from math import pi
 import numpy as np
 
-class psi_class:
-    def __init__(self, z, k):
-        self.z = np.array(z) # Composition
-        self.k = np.array(k) # K - equilibrium
-        
-    def f_psi(self, psi): # Function of psi -> 0 
-        a = np.sum((self.z*(1 - self.k))/(1+psi*(self.k - 1)))
-        return a
-
-
 #####################################################
+# Main Flash Code
 class Flash(MAiNGOmodel):
+    class psi_class:
+        def __init__(self):
+            self.z = None # Composition
+            self.k = None # K - equilibrium
+            
+        def set_values(self, z, k):
+            self.z = np.array(z)
+            self.k = np.array(k)
+            
+        def f_psi(self, psi): # Function of psi -> 0 
+            a = np.sum((self.z*(1 - self.k))/(1+psi*(self.k - 1)))
+            return a
+
     def __init__(self):
         MAiNGOmodel.__init__(self) # Should be there for technical reasons
-        z = [0.2, 0.8] # Composition
-        k = [1 , 0.5]  # Equilibrium
-        self.ext = psi_class(z, k)
+        self.ext = self.psi_class()
     
     # We need to implement the get_variables functions for specifying the optimization variables
     def get_variables(self):
-
         variables = [OptimizationVariable(Bounds(0,1), VT_CONTINUOUS, "Objective") ]    
         return variables
 
     def get_initial_point(self):
         initialPoint = [0.5] # Initial value of psi = 0.5
         return initialPoint
-
 
     def evaluate(self, vars):
         # Create copies of the variables with nicer names
@@ -48,10 +47,15 @@ class Flash(MAiNGOmodel):
         result.output = [OutputVariable("Value of psi: ", temp1)]
 
         return result
-
 #####################################################
 # To work with the problem, we first create an instance of the model.
 myModel = Flash()
+
+# Set the values of z and k
+z = [0.2, 0.8] # Composition
+k = [1 , 0.5]  # Equilibrium
+
+myModel.ext.set_values(z, k)
 
 # We then create an instance of MAiNGO, the solver, and hand it the model.
 myMAiNGO = MAiNGO(myModel)
@@ -61,3 +65,4 @@ myMAiNGO.read_settings(fileName) # If fileName is empty, MAiNGO will attempt to 
 
 maingoStatus = myMAiNGO.solve() 
 #print(maingoStatus)
+
