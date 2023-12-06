ab18f9f5dfa2e71a2ea9657b7724f99f33a1b1f5
JosePaor <paredesortizjosealfredo@gmail.com>
2023-12-05 19:45:10 -0600

    Correction of Psi
    Correction of Psi model, equation.
    outputs simplification

--- flash_base.py
+++ flash_base.py
@@ -1,5 +1,5 @@
 from maingopy import *
-from math import pi
+#from math import pi
 import numpy as np
 
 #####################################################
@@ -15,7 +15,7 @@ class Flash(MAiNGOmodel):
             self.k = np.array(k)
             
         def f_psi(self, psi): # Function of psi -> 0 
-            a = np.sum((self.z*(1 - self.k))/(1+psi*(self.k - 1)))
+            a = (np.sum((self.z*(1 - self.k))/(1+psi*(self.k - 1))))**2
             return a
 
     def __init__(self):
@@ -24,7 +24,7 @@ class Flash(MAiNGOmodel):
     
     # We need to implement the get_variables functions for specifying the optimization variables
     def get_variables(self):
-        variables = [OptimizationVariable(Bounds(0,1), VT_CONTINUOUS, "Objective") ]    
+        variables = [OptimizationVariable(Bounds(0,1), VT_CONTINUOUS, "psi") ]    
         return variables
 
     def get_initial_point(self):
@@ -35,34 +35,37 @@ class Flash(MAiNGOmodel):
         # Create copies of the variables with nicer names
         psi = vars[0]
         # Any variables defined here are intermediates that are not optimization variables.
-        temp1 = self.ext.f_psi(psi) # psi function
+        #temp1 =  # psi function
         
         # The objective and constraints are returned in an EvaluationContainer
         result = EvaluationContainer()
         
         # Function psi
-        result.objective = temp1
+        result.objective = self.ext.f_psi(psi)
         
-        # Additional output can be used to access intermediate variables after a problem has been solved.
-        result.output = [OutputVariable("Value of psi: ", temp1)]
-
         return result
 #####################################################
-# To work with the problem, we first create an instance of the model.
-myModel = Flash()
 
-# Set the values of z and k
-z = [0.2, 0.8] # Composition
-k = [1 , 0.5]  # Equilibrium
+def solve_flash(z, k):
+    # To work with the problem, we first create an instance of the model.
+    myModel = Flash()
 
-myModel.ext.set_values(z, k)
+    # Set the values of z and k
+    myModel.ext.set_values(z, k)
 
-# We then create an instance of MAiNGO, the solver, and hand it the model.
-myMAiNGO = MAiNGO(myModel)
+    # We then create an instance of MAiNGO, the solver, and hand it the model.
+    myMAiNGO = MAiNGO(myModel)
 
-fileName = ""
-myMAiNGO.read_settings(fileName) # If fileName is empty, MAiNGO will attempt to open MAiNGOSettings.txt
+    fileName = ""
+    myMAiNGO.read_settings(fileName) # If fileName is empty, MAiNGO will attempt to open MAiNGOSettings.txt
+    myMAiNGO.set_option("loggingDestination", LOGGING_NONE) # Not generate a logfile
+    myMAiNGO.set_option('writeResultFile', False) # Not generate a txt results file
+    
+    myMAiNGO.solve()
+    maingoStatus = myMAiNGO.get_solution_point()
+    return maingoStatus
 
-maingoStatus = myMAiNGO.solve() 
-#print(maingoStatus)
-
+# Now you can call the function solve_flash with different values of z and k
+status = solve_flash([0.1,0.2,0.3,0.4], [4.2,1.75, 0.74, 0.34])
+print(status)
+#print(status)
